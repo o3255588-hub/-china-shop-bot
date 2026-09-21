94846e884ddd00049c6da732e72bd4f152512014
o3255588-hub <o3255588@gmail.com>
2026-09-22 00:28:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,6 +36,7 @@ PRODUCTS = {
     "A103": {
         "name": "Nike futbolka",
         "price": 199000,
+        "image": "A103_1.jpg",
         "colors": ["Qora", "Oq"],
         "sizes": ["M", "X", "XL", "XXL"],
     },
